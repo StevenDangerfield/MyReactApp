cb586c7ab691fe95fc05e4f1818e51382ba4eb2b
Steven Dangerfield <marskimmer@gmail.com>
2024-06-30 17:05:00 +1000

    added solution file

--- my-react-app-frontend/src/Pages/MainPage.js
+++ my-react-app-frontend/src/Pages/MainPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import '../App.css';
 import Navigation from '../Components/Navigation';
 import WeatherReport from '../Components/WeatherReport';
+import Phaser from "phaser"
 
 function MainPage() {
   return (
