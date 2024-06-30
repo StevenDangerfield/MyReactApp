dbabe3b7d773969f81f210d9c614b3c79204b1e4
Steven Dangerfield <marskimmer@gmail.com>
2024-06-30 17:40:41 +1000

    add Fetch Data button

--- my-react-app-frontend/src/Components/WeatherReport.js
+++ my-react-app-frontend/src/Components/WeatherReport.js
@@ -2,14 +2,15 @@ import React, { useState, useEffect } from "react";
 import { getData } from "../api";
 import ship from "./Ship-Triangle-0001.png"
 
-function WeaterReport() {
+function WeatherReport() {
   const [data, setData] = useState([]);
 
+  async function fetchData() {
+    const result = await getData();
+    setData(result);
+  }
+
   useEffect(() => {
-    async function fetchData() {
-      const result = await getData();
-      setData(result);
-    }
     fetchData();
   }, []);
 
@@ -22,8 +23,11 @@ function WeaterReport() {
             </ul>
         ))}
         <img src={ship} alt="ship"/>
+
+        <button onClick={fetchData}>Fetch Data</button>
+        
     </div>
   );
 }
 
-export default WeaterReport;
+export default WeatherReport;
