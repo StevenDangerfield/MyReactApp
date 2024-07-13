93f38cbdc8399b39e90db27249d95d25f612640e
Steven Dangerfield <steven.dangerfield@workingmouse.com.au>
2024-07-13 16:05:41 +1000

    remove unused imports

--- my-react-app-frontend/src/Pages/MainPage.js
+++ my-react-app-frontend/src/Pages/MainPage.js
@@ -1,8 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import '../App.css';
 import Navigation from '../Components/Navigation';
 import WeatherReport from '../Components/WeatherReport';
-import Phaser from "phaser"
 
 function MainPage() {
   return (
